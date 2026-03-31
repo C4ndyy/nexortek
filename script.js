cc143047fe2a7796f06abd91225132cf82f508e9
C4ndyy <franciscoalvesteixeira30@gmail.com>
2026-03-31 16:37:29 +0100

    Fix contact v2

--- script.js
+++ script.js
@@ -216,7 +216,7 @@ document.addEventListener("DOMContentLoaded", () => {
             turnstileToken: token,
         };
 
-        const response = await fetch("/api/contact", {
+        const response = await fetch("/contact", {
             method: "POST",
             headers: {
             "Content-Type": "application/json",
